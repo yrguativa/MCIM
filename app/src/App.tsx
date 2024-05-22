0b3df3a4b7b8f1de2c7135c8ad10d9ec746cd936
Yilmer Ramiro Guativa Rincon <yilmer.guativa@siigo.com>
2024-05-22 14:40:11 -0500

    feat(App): :construction: 🫓 First component of ui shadcn

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+
+import { Input } from "@/components/ui/input"
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -28,6 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Input type="email" placeholder="Email" />
+
     </>
   )
 }
