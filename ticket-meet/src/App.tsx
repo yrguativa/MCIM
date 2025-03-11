742a20a1dfb50bcd87df011d42f05631a14aec03
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2025-03-11 17:19:59 -0500

    fix(Ticket Meet): :rocket: Fix manual replace

--- ticket-meet/src/App.tsx
+++ ticket-meet/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     // <BrowserRouter >
-    <BrowserRouter basename="/MCIM">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<GenerateTicket />} />
       </Routes>
