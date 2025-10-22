7c057dd89d6a9df16cb0284ed126548edfc73d2c
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2025-10-22 16:55:06 -0500

    feat(App): :bug: load ministries by page event register public

--- app/src/public/pages/EventRegister.tsx
+++ app/src/public/pages/EventRegister.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import ScanQR from "@/src/events/pages/ScanQR";
 
 import iconUrl from "@/src/assets/mci.svg";
+import { useMinistryStore } from "@/src/ministries/store/ministries.store";
 
 const EventRegisterPage: React.FC = () => {
+    const { getMinistries } = useMinistryStore();
     const { t } = useTranslation();
+
+    useEffect(() => {
+        getMinistries();
+    }, [getMinistries]);
+    
     return (
         <div className=" min-h-svh flex-row items-center justify-center pt-4">
-            <div className="flex items-center m-auto ml-8 justify-center">
+            <div className="flex items-center m-auto justify-center">
                 <img src={iconUrl} width="50" height="50" alt="MCI Icon" />
                 <h1 className="text-3xl font-bold mt-2 ml-4">
                     {t('AppTitle')}
